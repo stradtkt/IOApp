cc282887ae3567f205d2d3da4b3f8fc76a5ce02c
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-08 14:34:11 -0500

    Added HomeOrAway Enum

---
+++ IOApp/IOApp/GameResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOApp
+{
+    class GameResult
+    {
+        public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeOrAway { get; set; }
+    }
+    public enum HomeOrAway
+    {
+        Home,
+        Away
+    }
+}
--- IOApp/IOApp/Program.cs
+++ IOApp/IOApp/Program.cs
@@ -22,16 +22,29 @@ namespace IOApp
             }
         }
 
-        public static List<string[]> ReadSoccerResults(string fileName)
+        public static List<GameResult> ReadSoccerResults(string fileName)
         {
-            var soccerResults = new List<string[]>();
+            var soccerResults = new List<GameResult>();
             using (var reader = new StreamReader(fileName))
             {
                 string line = "";
+                reader.ReadLine();
                 while((line = reader.ReadLine()) != null)
                 {
+                    var gameResult = new GameResult();
                     string[] values = line.Split(',');
-                    soccerResults.Add(values);
+                    DateTime gameDate;
+                    if(DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if(Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    soccerResults.Add(gameResult);
                 }
             }
             return soccerResults;
